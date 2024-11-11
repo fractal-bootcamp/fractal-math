a9a93548b31a628a9bb4d440eadc5637ccf6b36d
cecepizza <pizzacort@iivi.co>
2024-11-11 15:03:30 -0500

    authentication with NextJs implemented

---
+++ src/lib/auth.ts
@@ -0,0 +1,13 @@
+import { AuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+// Import other providers as needed
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    // Add other providers here
+  ],
+};
