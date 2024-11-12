b615817c3b278b17ed41bc427c0166abcbd549a1
Isaac Gemal <isaacgemal@gmail.com>
2024-11-12 16:30:49 -0500

    🐛 Fix: disable eslint for var in prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
-// Prevent multiple instances of Prisma Client in development
 declare global {
+  // Only var creates properties on the global object, used to prevent multiple Prisma instances in dev
+  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
 const prisma = global.prisma || new PrismaClient();
-
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
 }
